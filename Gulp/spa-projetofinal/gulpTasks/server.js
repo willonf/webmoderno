0f2194632405865b0c84a6a49eaa0979fd12f749
Willon <contatowillon@gmail.com>
2021-01-11 22:53:20 -0400

    Commit

--- Gulp/spa-projetofinal/gulpTasks/server.js
+++ Gulp/spa-projetofinal/gulpTasks/server.js
@@ -1,10 +1,23 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
-function monitorarArquivos(cb) {
-    return cb()
+function monitorarArquivos() {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080, // Indica a porta onde será executado o servidor
+            open: true, // Abertura no navegador
+            livereload: true // Recarregar automaticamente sempre que houver mudança no código
+        }))
 }
 
 function servidor(cb) {
+    // Indicar o que deve ser observado e o que deve ser feito caso haja alterações
+    watch(('./src/**/*.html'), () => gulp.series('appHTML')()) 
+    watch(('./src/**/*.scss'), () => gulp.series('appCSS')()) 
+    watch(('./src/**/*.js'), () => gulp.series('appCSS')()) 
+    watch(('./src/assets/imgs/**/*.*'), () => gulp.series('appIMG')()) 
+
     return cb()
 }
 
